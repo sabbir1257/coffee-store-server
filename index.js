25eede5c1c160077f40faccd0b32bdf26e0d9f1b
sabbir1257 <masabbir1257@gmail.com>
2025-03-04 11:49:32 +0600

    file push

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,11 +12,10 @@ app.use(express.json());
 // coffeeMaster
 // gmqj7rzM4LaLcIcL
 
-const uri =
-  "mongodb+srv://coffeeMaster:gmqj7rzM4LaLcIcL@cluster0.ury0v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-/* const uri =
-  "mongodb+srv://<db_username>:<db_password>@cluster0.ury0v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
- */
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASS);
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ury0v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
